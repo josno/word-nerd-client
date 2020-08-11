62160dc9bf9b71d39e7115d830b359a13875e91e
josno <jnsenoren@gmail.com>
2020-08-11 21:45:18 +0400

    Update for new component

--- src/components/passtheball/PassTheBall.js
+++ src/components/passtheball/PassTheBall.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import Ball from './giphy.gif';
-import './PassTheBall.css';
-import Sound from 'react-sound';
-import soundfile from './pass-song.mp3';
-import PassTheBallMessage from '../PassTheBallMessage/PassTheBallMessage';
+import React, { Component } from "react";
+import Ball from "./giphy.gif";
+import "./PassTheBall.css";
+import Sound from "react-sound";
+import soundfile from "./pass-song.mp3";
+import HeaderMessage from "../HeaderMessage/HeaderMessage";
 
 class PassTheBall extends Component {
 	render() {
 		return (
-			<section className="game-play-container">
-				<PassTheBallMessage />
+			<section className='game-play-container'>
+				<HeaderMessage oldMessage='Start!' newMessage='Pass The Ball' />
 				<Sound
 					url={soundfile}
 					playStatus={Sound.status.PLAYING}
 					onFinishedPlaying={this.handleSongFinishedPlaying}
 				/>
-				<div className="ball-image-container">
-					<img
-						className="ball-gif"
-						alt="ball-placeholder"
-						src={Ball}
-					/>
+				<div className='ball-image-container'>
+					<img className='ball-gif' alt='ball-placeholder' src={Ball} />
 				</div>
 			</section>
 		);
