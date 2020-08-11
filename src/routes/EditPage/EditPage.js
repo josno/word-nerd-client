4fed55b375662aa6611ae065951f8c04aafed8f9
josno <jnsenoren@gmail.com>
2020-08-11 18:29:09 +0400

    Refactor for hooks

--- src/routes/EditPage/EditPage.js
+++ src/routes/EditPage/EditPage.js
@@ -1,148 +1,123 @@
-import React, { Component } from 'react';
-import './EditPage.css';
-import ValidationMessage from '../../components/ValidationMessage/ValidationMessage';
-import GamesService from '../../services/api-service';
+import React, { useState, useEffect } from "react";
+import "./EditPage.css";
+import ValidationMessage from "../../components/ValidationMessage/ValidationMessage";
+import GamesService from "../../services/api-service";
 
-class EditPage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			wordList: [],
-			title: '',
-			updatedTitle: false,
-			updatedInput: false
-		};
-		this.validateTitle = this.validateTitle.bind(this);
-		this.validateInput = this.validateInput.bind(this);
-		this.updateInput = this.updateInput.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
+const EditPage = (props) => {
+	const [wordList, setWordList] = useState([]);
+	const [title, setTitle] = useState("");
+	const [updatedTitle, setUpdatedTitle] = useState(false);
+	const [updatedInput, setUpdatedInput] = useState(false);
 
-	updateInput(text) {
-		this.setState({
-			wordList: text,
-			updatedInput: true
-		});
-	}
+	const updateInput = (text) => {
+		setWordList(text);
+		setUpdatedInput(true);
+	};
 
-	updateTitle(text) {
-		this.setState({
-			title: text,
-			updatedTitle: true
-		});
-	}
+	const updateTitle = (text) => {
+		setTitle(text);
+		setUpdatedTitle(true);
+	};
 
-	validateInput() {
-		const wordInput = this.state.wordList.toString();
+	const validateInput = () => {
+		const wordInput = wordList.toString();
 
 		const invalidChar = wordInput.match(/[^a-zA-Z,\s]/g);
 
 		if (invalidChar) {
-			return 'Type only letters please.';
+			return "Type only letters please.";
 		}
-	}
+	};
 
-	validateTitle() {
-		const titleInput = this.state.title;
+	const validateTitle = () => {
+		const titleInput = title;
 		const invalidChar = titleInput.match(/[^a-zA-Z,\s]/g);
 
 		if (invalidChar) {
-			return 'Type only letters please.';
+			return "Type only letters please.";
 		}
-	}
+	};
 
-	handleSubmit(e) {
+	const handleSubmit = (e) => {
 		e.preventDefault();
-		const { title, wordList } = this.state;
 
-		const trimmedWordList = wordList.replace(/,\s*$/, '');
+		const trimmedWordList = wordList.replace(/,\s*$/, "");
 
 		const wordListArray =
-			typeof trimmedWordList !== 'string'
+			typeof trimmedWordList !== "string"
 				? trimmedWordList
-				: trimmedWordList.split(',').map(i => i.trim());
+				: trimmedWordList.split(",").map((i) => i.trim());
 
 		const newGame = {
 			title: title,
-			word_list: wordListArray
+			word_list: wordListArray,
 		};
 
-		GamesService.updateGame(newGame, this.props.gameId).then(response => {
-			console.log(response);
-			this.props.history.push(`/game-home-page`);
+		GamesService.updateGame(newGame, props.gameId).then((response) => {
+			props.history.push(`/game-home-page`);
 		});
-	}
+	};
 
-	componentDidMount() {
-		GamesService.getGameContent(this.props.gameId).then(responsejson => {
-			const words = [...responsejson.word_list].join(', ');
-			this.setState({
-				wordList: words,
-				title: responsejson.title
-			});
+	useEffect(() => {
+		GamesService.getGameContent(props.gameId).then((responsejson) => {
+			const words = [...responsejson.word_list].join(", ");
+			setWordList(words);
+			setTitle(responsejson.title);
 		});
-	}
+	}, [props.gameId]);
 
-	render(props) {
-		const titleError = this.validateTitle();
-		const inputError = this.validateInput();
+	const titleError = validateTitle();
+	const inputError = validateInput();
 
-		return (
-			<div>
-				<section>
-					<header className="input-instructions">
-						<h1>Type Words into the Box Separated by Commas</h1>
-					</header>
-					<form>
-						<input
-							className="title-name"
-							value={this.state.title}
-							onChange={e => this.updateTitle(e.target.value)}
+	return (
+		<div>
+			<section>
+				<header className='input-instructions'>
+					<h1>Type Words into the Box Separated by Commas</h1>
+				</header>
+				<form>
+					<input
+						className='title-name'
+						value={title}
+						onChange={(e) => updateTitle(e.target.value)}
+						required
+					/>
+
+					{updatedTitle && <ValidationMessage message={titleError} />}
+					<div className='input-container'>
+						<textarea
+							className='input-box'
+							aria-required='true'
+							value={wordList}
+							onChange={(e) => updateInput(e.target.value)}
 							required
 						/>
-
-						{this.state.updatedTitle && (
-							<ValidationMessage message={titleError} />
-						)}
-						<div className="input-container">
-							<textarea
-								className="input-box"
-								aria-required="true"
-								value={this.state.wordList}
-								onChange={e => this.updateInput(e.target.value)}
-								required
-							/>
-						</div>
-
-						{this.state.updatedInput && (
-							<ValidationMessage message={inputError} />
-						)}
-					</form>
-					<div className="submit-button-container">
-						<button
-							aria-label="cancel"
-							className="submit-cancel"
-							onClick={() => this.props.history.goBack()}
-						>
-							Cancel
-						</button>
-
-						<button
-							aria-label="update"
-							className="submit-input"
-							type="submit"
-							disabled={
-								this.validateInput() || this.validateTitle()
-							}
-							onClick={this.handleSubmit}
-						>
-							Update
-						</button>
 					</div>
-				</section>
-			</div>
-		);
-	}
-}
+
+					{updatedInput && <ValidationMessage message={inputError} />}
+				</form>
+				<div className='submit-button-container'>
+					<button
+						aria-label='cancel'
+						className='submit-cancel'
+						onClick={() => props.history.goBack()}
+					>
+						Cancel
+					</button>
+
+					<button
+						aria-label='update'
+						className='submit-input'
+						type='submit'
+						disabled={validateInput() || validateTitle()}
+						onClick={(e) => handleSubmit(e)}
+					>
+						Update
+					</button>
+				</div>
+			</section>
+		</div>
+	);
+};
 
 export default EditPage;
