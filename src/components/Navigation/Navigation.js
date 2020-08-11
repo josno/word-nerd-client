9b5b82e14c5d153636cdd5c2f8add8ea8770be7f
josno <jnsenoren@gmail.com>
2020-08-11 22:08:24 +0400

    Update

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './Navigation.css';
-import TokenService from '../../services/token-service';
-import WordContext from '../../WordContext';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+import TokenService from "../../services/token-service";
+import WordContext from "../../WordContext";
 
 class Navigation extends Component {
 	static contextType = WordContext;
 
 	handleLogoutClick = () => {
-		TokenService.clearAuthToken('word-nerd-token');
+		TokenService.clearAuthToken("word-nerd-token");
 		this.context.handleLogInState();
 	};
 
 	renderLogoutLink() {
 		return (
 			<>
-				<Link className="link-text" to="/game-home-page">
+				<Link className='link-text' to='/game-home-page'>
 					My Page
 				</Link>
-				<Link
-					className="link-text"
-					onClick={this.handleLogoutClick}
-					to="/"
-				>
+				<Link className='link-text' onClick={this.handleLogoutClick} to='/'>
 					Logout
 				</Link>
 			</>
@@ -31,8 +27,8 @@ class Navigation extends Component {
 
 	renderLoginLink() {
 		return (
-			<div className="not-logged-in">
-				<Link className="link-text" to="/">
+			<div className='not-logged-in'>
+				<Link className='link-text' to='/'>
 					Log in
 				</Link>
 			</div>
@@ -41,12 +37,12 @@ class Navigation extends Component {
 
 	render() {
 		return (
-			<nav className="responsive-nav-wrapper">
-				<label htmlFor="toggle">&#9776;</label>
-				<input type="checkbox" id="toggle" />
-				<div className="menu">
-					<Link id="nav-title" to="/">
-						PawPad
+			<nav className='responsive-nav-wrapper'>
+				<label htmlFor='toggle'>&#9776;</label>
+				<input type='checkbox' id='toggle' />
+				<div className='menu'>
+					<Link id='nav-title' to='/'>
+						Word Nerd
 					</Link>
 					{TokenService.hasAuthToken()
 						? this.renderLogoutLink()
